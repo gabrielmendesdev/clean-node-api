e9694e7111a92f3d21f5078352c8a61d884134d7
Gabriel Mendes da Silva <122908813+GabrielMendes06@users.noreply.github.com>
2024-03-20 12:45:50 -0300

    refactor: disable this coverage files

--- jest.config.ts
+++ jest.config.ts
@@ -7,7 +7,7 @@ import type {Config} from 'jest';
 
 const config: Config = {
   roots: ['<rootDir>/src'],
-  collectCoverage: true,
+  collectCoverage: false,
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: "coverage",
   testEnvironment: 'node',
